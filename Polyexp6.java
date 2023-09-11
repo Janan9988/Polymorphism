6d538d6e8dd9b50d0a6cdbe08e823d8e927a1ad4
janan <114799522+Janan9988@users.noreply.github.com>
2023-09-11 22:46:43 +0530

    Add files via upload

---
+++ Polyexp6.java
@@ -0,0 +1,17 @@
+// overloding
+class Adder{
+    static int add(int a, int b){
+        return a+b;
+    }
+    static int  add(int a, int b, int c){
+        return a+b+c;
+    }
+}
+ public class Polyexp6{
+    public static void main(String[]args){
+        System.out.println(Adder.add(11,11));
+        System.out.println(Adder.add(11,11,11));
+
+    }
+
+}
